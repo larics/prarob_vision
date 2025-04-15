3491eb5779392fb5a811e2d9ba10cab36ce27e0a
fpetric <f5r1c.1m0@gmail.com>
2025-04-15 15:57:18 +0200

    Update launch for ROS2

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/images', ['images/image.PNG'])
+        ('share/' + package_name + '/images', ['images/image.PNG']),
+        ('share/' + package_name + '/launch', ['launch/image_process.launch.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
